63c96185f4ce65a10671d004a8a396501bbf56fa
Dias <98723703+Nrdias@users.noreply.github.com>
2024-04-02 12:58:44 -0300

    Update MoneyTracker.java

--- trab-I/src/MoneyTracker.java
+++ trab-I/src/MoneyTracker.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.*;
 
@@ -9,6 +10,9 @@ public class MoneyTracker {
     private static final char VERTICAL = '|';
     private static final char HORIZONTAL = '-';
     private static final char BANDITS_CAPTURED = '#';
+
+    private static int operations = 0;
+    private static int money = 0;
 
     private static class Pointer {
         int x;
@@ -32,109 +36,69 @@ public class MoneyTracker {
         }
     }
 
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                digite o numero de entradas:
+                50
+                100
+                200
+                500
+                750
+                1000
+                1500
+                2000
+                """);
+        int entrada = scanner.nextInt();
         List<Pointer> pointers = new ArrayList<>();
-        String path = "src/files/casoG50.txt";
+        Pointer previous = null;
+        Path path = Paths.get("src/files/casoG" + entrada +".txt");
         List<Pointer> navigation = new ArrayList<>();
-        Pointer previousPointer = null;
         try {
-            List<String> lines = Files.readAllLines(Paths.get(path));
-            for (int i = 0; i < lines.size(); i++) {
-                String line = lines.get(i);
-                for (int j = 0; j < line.length(); j++) {
-                    char c = line.charAt(j);
-                    if (c != ' ') pointers.add(new Pointer(i, j, c));
+            BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset());
+            String line;
+            int y = 0;
+            while ((line = reader.readLine()) != null) {
+                for (int x = 0; x < line.length(); x++) {
+                    char content = line.charAt(x);
+                    if (content != ' ') {
+                        Pointer pointer = new Pointer(y, x, content);
+                        pointers.add(pointer);
+                        previous = pointer;
+                        operations++;
+                    }
                 }
+                y++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        for (int i = 0; i < pointers.size(); i++) {
-            Pointer pointer = pointers.get(i);
-            if (previousPointer != null) {
-                previousPointer.next = pointer;
-            }
-            previousPointer = pointer;
-        }
-
-
-        System.out.println(pointers);
         Pointer start = findStart(pointers);
+        navigation.add(start);
         Pointer current = start;
-        Pointer next = current.next;
-        Pointer previous = start;
-        navigation.add(current);
-        int directionX = 1;
-        int directionY = 0;
-
+        previous = start;
+        int directionX = 1; // 1 = right, -1 = left
+        int directionY = 0; // -1 = down, 1 = up
+        String moneyString = "";
         while (current.content != BANDITS_CAPTURED) {
-            System.out.println(current);
-            if (current.content == HORIZONTAL) {
-                if (next.content == VERTICAL) {
-                    current = next.next;
-                    continue;
-                }
-                if (previous.content == HORIZONTAL || previous.content == LEFT || previous.content == RIGHT || Character.isDigit(previous.content)) {
-                    if (directionX == 1) {
-                        for (Pointer pointer : pointers) {
-                            if (pointer.x == current.x && pointer.y == current.y + 1) {
-                                previous = current;
-                                current = pointer;
-                                previous.next = current;
-                                navigation.add(current);
-                                break;
-                            }
-                        }
-                    }
-                    if (directionX == -1) {
-                        for (Pointer pointer : pointers) {
-                            if (pointer.x == current.x && pointer.y == current.y - 1) {
-                                previous = current;
-                                current = pointer;
-                                previous.next = current;
-                                navigation.add(current);
-                                break;
-                            }
-                        }
-                    }
-                }
-
-            }
-            if (current.content == VERTICAL) {
-                if (next.content == HORIZONTAL) {
-                    current = next.next;
-                    continue;
-                }
-                if (previous.content == VERTICAL || previous.content == LEFT || previous.content == RIGHT || Character.isDigit(previous.content)) {
-                    if (directionY == 1) {
-                        for (Pointer pointer : pointers) {
-                            if (pointer.x == current.x + 1 && pointer.y == current.y) {
-                                previous = current;
-                                current = pointer;
-                                previous.next = current;
-                                navigation.add(current);
-                                break;
-                            }
-                        }
-                    }
-                    if (directionY == -1) {
-                        for (Pointer pointer : pointers) {
-                            if (pointer.x == current.x - 1 && pointer.y == current.y) {
-                                previous = current;
-                                current = pointer;
-                                previous.next = current;
-                                navigation.add(current);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
             if (current.content == LEFT) {
                 if (directionX == 1) {
                     directionX = 0;
+                    directionY = -1;
+                } else if (directionX == -1) {
+                    directionX = 0;
+                    directionY = 1;
+                } else if (directionY == 1) {
+                    directionX = -1;
+                    directionY = 0;
+                } else if (directionY == -1) {
+                    directionX = 1;
+                    directionY = 0;
+
+                }
+            } else if (current.content == RIGHT) {
+                if (directionX == 1) {
+                    directionX = 0;
                     directionY = 1;
                 } else if (directionX == -1) {
                     directionX = 0;
@@ -146,93 +110,35 @@ public class MoneyTracker {
                     directionX = -1;
                     directionY = 0;
                 }
-                for (Pointer pointer : pointers) {
-                    if (pointer.x == current.x + directionX && pointer.y == current.y + directionY) {
-                        previous = current;
-                        current = pointer;
-                        previous.next = current;
-                        navigation.add(current);
-                        break;
-                    }
-                }
             }
-            if (current.content == RIGHT) {
-                if (directionX == 1) {
-                    directionX = 0;
-                    directionY = -1;
-                } else if (directionX == -1) {
-                    directionX = 0;
-                    directionY = 1;
-                } else if (directionY == 1) {
-                    directionX = -1;
-                    directionY = 0;
-                } else if (directionY == -1) {
-                    directionX = 1;
-                    directionY = 0;
-                }
-                for (Pointer pointer : pointers) {
-                    if (pointer.x == current.x + directionX && pointer.y == current.y + directionY) {
-                        previous = current;
-                        current = pointer;
-                        previous.next = current;
-                        navigation.add(current);
-                        break;
-                    }
-                }
-            }
-            if (Character.isDigit(current.content)) {
-                if (directionX == 1) {
-                    for (Pointer pointer : pointers) {
-                        if (pointer.x == current.x && pointer.y == current.y + 1) {
-                            previous = current;
-                            current = pointer;
-                            previous.next = current;
-                            navigation.add(current);
-                            break;
+            for (Pointer pointer : pointers) {
+                if (pointer.x == current.x + directionY && pointer.y == current.y + directionX) {
+                    navigation.add(pointer);
+                    previous = current;
+                    current = pointer;
+                    if (Character.isDigit(current.content)) {
+                        moneyString += current.content;
+                    }else{
+                        if (!moneyString.isEmpty()) {
+                            money += Integer.parseInt(moneyString);
+                            moneyString = "";
                         }
                     }
+                    operations++;
+                    break;
                 }
-                if (directionX == -1) {
-                    for (Pointer pointer : pointers) {
-                        if (pointer.x == current.x && pointer.y == current.y - 1) {
-                            previous = current;
-                            current = pointer;
-                            previous.next = current;
-                            navigation.add(current);
-                            break;
-                        }
-                    }
-                }
-                if (directionY == 1) {
-                    for (Pointer pointer : pointers) {
-                        if (pointer.x == current.x + 1 && pointer.y == current.y) {
-                            previous = current;
-                            current = pointer;
-                            previous.next = current;
-                            navigation.add(current);
-                            break;
-                        }
-                    }
-                }
-                if (directionY == -1) {
-                    for (Pointer pointer : pointers) {
-                        if (pointer.x == current.x - 1 && pointer.y == current.y) {
-                            previous = current;
-                            current = pointer;
-                            previous.next = current;
-                            navigation.add(current);
-                            break;
-                        }
-                    }
-                }
+
             }
         }
+        System.out.println("Entradas:" + entrada + ", Operações: " + operations);
+        System.out.println("Dinheiro: " + money);
     }
 
-public static Pointer findStart(List<Pointer> pointers) {
-    for (Pointer pointer : pointers) {
-        if (pointer.y == 0 && pointer.content == '-') return pointer;
+
+    public static Pointer findStart(List<Pointer> pointers) {
+        for (Pointer pointer : pointers) {
+            if (pointer.y == 0 && pointer.content == '-') return pointer;
+        }
+        return null;
     }
-    return null;
-}
 }
